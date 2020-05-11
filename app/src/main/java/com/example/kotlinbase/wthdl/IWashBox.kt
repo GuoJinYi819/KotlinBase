a5995d63309cc54fc72ec1a551039cd3ebe4f914
GuoJinYi819 <gjy2000@>
2020-05-11 19:05:05 +0800

    05-11：代理和委托

---
+++ app/src/main/java/com/example/kotlinbase/wthdl/IWashBox.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinbase.wthdl
+
+/**ClassName: KotlinBase
+ * @author 作者 : GuoJinYi
+ * @version 创建时间：2020/5/11 0011 18:56
+ * @Description: 用途：完成特定功能
+ */
+interface IWashBow{
+    fun washing()
+}
---
+++ app/src/main/java/com/example/kotlinbase/wthdl/Test.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinbase.wthdl
+
+/**ClassName: KotlinBase
+ * @author 作者 : GuoJinYi
+ * @version 创建时间：2020/5/11 0011 18:58
+ * @Description: 用途：完成特定功能
+ */
+fun main() {
+
+    var father = SmallHeahFather()
+    father.washing()
+}
