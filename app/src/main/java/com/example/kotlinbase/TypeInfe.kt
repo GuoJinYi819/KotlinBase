d7b85f1b769237dbb6b5e31b12ad27ad639a9f04
GuoJinYi819 <gjy2000@>
2020-05-11 14:07:47 +0800

    05-11：变量

---
+++ app/src/main/java/com/example/kotlinbase/TypeInfe.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinbase
+
+/**ClassName: KotlinBase
+ * @author 作者 : GuoJinYi
+ * @version 创建时间：2020/5/11 0011 13:46
+ * @Description: 用途：完成特定功能
+ */
+class TypeInfe {
+    //智能类型推断
+    var i=18;
+    var str = "撒旦"
+    //var 声明变量
+
+    val a = "刑天铠甲"
+    //val声明常量 不可被重新赋值
+    // !!! 不可以的  a="hhh"
+
+    //kotlin 会通过类型推断自动数据类型，也可以显示的指定数据类型
+
+
+}
