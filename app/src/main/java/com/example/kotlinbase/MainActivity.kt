545ce23a4a910b9738c14e8e583a15d3d2f9d067
GuoJinYi819 <gjy2000@>
2020-05-11 14:45:09 +0800

    05-11：if流程控制

--- app/src/main/java/com/example/kotlinbase/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbase/MainActivity.kt
@@ -21,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         val diaryGenerater = diaryGenerater("李白")
         println(diaryGenerater)
 
+        checkFace(100)
+
     }
     //日记生成方法
     fun diaryGenerater(placeName:String):String{
@@ -28,4 +30,15 @@ class MainActivity : AppCompatActivity() {
         var temple = "唐代：${placeName}  ---白日依山尽，长河落日圆"
         return temple;
     }
+
+    //条件控制   颜值判断器
+    fun checkFace(score:Int){
+        if (score>80){
+            //字符串模板
+            println("分数${score}")
+        }else{
+            println("分数${score}")
+        }
+    }
+
 }
